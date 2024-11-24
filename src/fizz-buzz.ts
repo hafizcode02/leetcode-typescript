9337922f68a9b80351c50c68532f4f07428044f1
Hafiz Caniago <hafizcode02@gmail.com>
2024-11-24 14:20:14 +0700

    feat: update some description

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,26 +1,30 @@
-// Given an integer n, return a string array answer (1-indexed) where:
+/**
+ * 412. Fizz Buzz (https://leetcode.com/problems/fizz-buzz)
+ * 
+ * Given an integer n, return a string array answer (1-indexed) where:
 
-// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-// answer[i] == "Fizz" if i is divisible by 3.
-// answer[i] == "Buzz" if i is divisible by 5.
-// answer[i] == i (as a string) if none of the above conditions are true.
+ * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ * answer[i] == "Fizz" if i is divisible by 3.
+ * answer[i] == "Buzz" if i is divisible by 5.
+ * answer[i] == i (as a string) if none of the above conditions are true.
 
-// Example 1:
+ * Example 1:
 
-// Input: n = 3
-// Output: ["1","2","Fizz"]
-// Example 2:
+ * Input: n = 3
+ * Output: ["1","2","Fizz"]
+ * Example 2:
 
-// Input: n = 5
-// Output: ["1","2","Fizz","4","Buzz"]
-// Example 3:
+ * Input: n = 5
+ * Output: ["1","2","Fizz","4","Buzz"]
+ * Example 3:
 
-// Input: n = 15
-// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ * Input: n = 15
+ * Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
 
-// Constraints:
+ * Constraints:
 
-// 1 <= n <= 104
+ * 1 <= n <= 104
+*/
 
 function fizzBuzz(n: number): string[] {
   const result: string[] = [];
@@ -39,27 +43,6 @@ function fizzBuzz(n: number): string[] {
 
   return result;
 }
-
-// function fizzBuzz(n: number): string[] {
-//     const list = [];
-
-//     for (let i = 1; i <= n; i++) {
-//         let myWord = ''
-//         if (i % 3 === 0) {
-//             myWord += 'Fizz'
-//         }
-//         if (i % 5 === 0) {
-//             myWord += 'Buzz'
-//         }
-
-
-//         myWord ||= `${i}`;
-
-//         list.push(myWord)
-
-//     }
-//     return list;
-// };
 
 // Example usage:
 console.log(fizzBuzz(3)); // Output: ["1", "2", "Fizz"]
--- src/longest-valid-parentheses.ts
+++ src/longest-valid-parentheses.ts
@@ -1,24 +1,28 @@
-// Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses
-// substring
-// .
+/**
+ * 32. Longest Valid Parentheses (32. Longest Valid Parentheses)
+ * 
+ * Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses
+ * substring
+ * .
 
-// Example 1:
-//      Input: s = "(()"
-//      Output: 2
-//      Explanation: The longest valid parentheses substring is "()".
+ * Example 1:
+ *      Input: s = "(()"
+ *      Output: 2
+ *      Explanation: The longest valid parentheses substring is "()".
 
-// Example 2:
-//      Input: s = ")()())"
-//      Output: 4
-//      Explanation: The longest valid parentheses substring is "()()".
+ * Example 2:
+ *      Input: s = ")()())"
+ *      Output: 4
+ *      Explanation: The longest valid parentheses substring is "()()".
 
-// Example 3:
-//      Input: s = ""
-//      Output: 0
+ * Example 3:
+ *      Input: s = ""
+ *      Output: 0
 
-// Constraints:
-//      0 <= s.length <= 3 * 104
-//      s[i] is '(', or ')'.
+ * Constraints:
+ *      0 <= s.length <= 3 * 104
+ *      s[i] is '(', or ')'.
+ */
 
 function longestValidParentheses(s: string): number {
   let maxLength = 0;
